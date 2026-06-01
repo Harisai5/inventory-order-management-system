52e73405f95bbc7f6cb85a9f9bcf4c778c5e00b1
Harisai5 <harisaiavinash5@gmail.com>
2026-06-01 14:08:45 +0530

    Migrated to PostgreSQL

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,13 +1,11 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import declarative_base
 
-DATABASE_URL = "sqlite:///./inventory.db"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False}
-)
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(
     autocommit=False,
